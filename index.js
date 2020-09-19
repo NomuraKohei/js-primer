a709b80dfbd54ca5944c96a05d6c52c3fd7b9f2e
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-09-19 22:56:53 +0900

    fix: promiseのリファクタ

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 function main() {
-    fetchUserInfo("js-primer-example");
+    fetchUserInfo("js-primer-example")
+        .then((userInfo) => createView(userInfo))
+        .then((view) => displayView(view))
+        .catch((error) => {
+            console.error(`エラーが発生しました(${error})`);
+        });
 }
 
 function fetchUserInfo(userId){
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then(response => {
-        if(!response.ok){
-            console.log("エラーレスポンス", response);
-        } else {
-            return response.json().then(userInfo => {
-                // JSONパースされたオブジェクトが渡される
-                const view = createView(userInfo);
-                displayView(view);
-            });
-        }
-    }).catch( error => {
-        console.log("error");
-        console.log(error);
-    } );
+    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            if(!response.ok){
+                return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+            } else {
+                return response.json();
+            }
+        });
 }
 
 function createView(userInfo) {
