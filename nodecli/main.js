5a7db41cd1b554c3470a33ff41f2f35ca66094b2
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-09-21 16:58:38 +0900

    feat: MarkdownをHTMLに変換する

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,8 +1,30 @@
-console.log("Hello World!");
-console.log(process.argv);
-
 const program = require("commander");
+const fs = require("fs");
+const { options } = require("marked");
+const marked = require("marked");
+
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
+
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+};
+
+
+console.log(cliOptions);
 
 const filePath = program.args[0];
 console.log(filePath);
+
+fs.readFile(filePath, { encoding: "utf8"} ,(err, file) => {
+    if(err) {
+        console.log(err.message);
+        process.exit(1);
+        return;
+    }
+    const html = marked(file, {
+        gfm: cliOptions.gfm,
+    });
+    console.log(html);
+});
